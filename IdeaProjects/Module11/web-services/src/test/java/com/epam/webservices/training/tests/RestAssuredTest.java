f7cc04b00bda8efefcb0da9ac090bfb5ff19d611
Boris Golomb <boris_golomb@epam.com>
2018-03-16 12:31:44 +0300

    Add test 2

---
+++ IdeaProjects/Module11/web-services/src/test/java/com/epam/webservices/training/tests/RestAssuredTest.java
@@ -0,0 +1,42 @@
+package com.epam.webservices.training.tests;
+
+import static io.restassured.RestAssured.given;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.epam.webservices.training.model.post.Post;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class RestAssuredDemoTest {
+
+	@BeforeTest
+	public void initTest() {
+		RestAssured.baseURI = "https://jsonplaceholder.typicode.com/users";
+	}
+	
+	@Test
+	public void checkStatusCode1() {
+		Response rp = given().get("/posts").andReturn();	
+		int actualStatusCode = rp.getStatusCode();
+		System.out.println(actualStatusCode);
+		Assert.assertEquals(actualStatusCode, 200);
+	}
+	
+	@Test
+	public void checkResponseHeader() {
+		Response rp = given().get("/posts").andReturn();	
+		String valueOfContentTypeHeader = rp.getHeader("content-type");
+		Assert.assertTrue(valueOfContentTypeHeader.contains("application/json"));
+	}
+	@Test
+	public void checkResponseBody() {
+		Response rp = given().get("/posts").andReturn();	
+		Post[] posts = rp.as(Post[].class);
+		Assert.assertEquals(posts.length, 100);
+	}
+	
+}
